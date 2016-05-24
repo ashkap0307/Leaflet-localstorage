509f286340e5c9852f0aaae405c6870becfff58d
rajeshdude0 <rajesh.sharma@tismotech.net>
2016-05-24 15:56:10 +0530

    Add files via upload

---
+++ work.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(0);
+// include class file
+include("Traceroute.php");
+
+// create object instance
+$tr = Net_Traceroute::factory();
+
+if(!PEAR::isError($tr)) 
+{
+	 $hostname = $_GET['hostname'];
+	 $flag = true;
+	 $data = array();
+      // trace route to host and get response
+      $response = $tr->traceroute($hostname);
+  
+   $data = $response;
+   $ip = array();
+
+foreach($data as $v){
+	if($flag)
+	   foreach($v as $s){
+	   $ip[]=$s['ip'];
+	   }
+	  $flag = false;
+	   
+}
+ 
+ 
+ 
+      // print response
+ echo json_encode($ip);
+}
+  
+?>
